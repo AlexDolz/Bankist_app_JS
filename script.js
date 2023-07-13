19925a2770fdbf8bb2089d0a6c266866dc60965d
AlexDolz <aleksejdolznikov3@gmail.com>
2023-07-13 12:51:42 +0200

    loan handler

--- script.js
+++ script.js
@@ -155,6 +155,21 @@ btnTransfer.addEventListener('click', e => {
   }
 });
 
+btnLoan.addEventListener('click', e => {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', e => {
   e.preventDefault();
   if (
